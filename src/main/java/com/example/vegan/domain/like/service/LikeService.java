7ecb1198762d4d2ab5cef63e2993d217e23f65e9
wheeeung <whee050916@gmail.com>
2022-09-10 04:45:44 +0900

    📑 :: edit like

--- src/main/java/com/example/vegan/domain/like/service/LikeService.java
+++ src/main/java/com/example/vegan/domain/like/service/LikeService.java
@@ -4,7 +4,9 @@ import com.example.vegan.domain.like.entity.Like;
 import com.example.vegan.domain.like.repository.LikeRepository;
 import com.example.vegan.domain.post.entity.Post;
 import com.example.vegan.domain.post.repository.PostRepository;
+import com.example.vegan.domain.user.entity.User;
 import com.example.vegan.domain.user.repository.UserRepository;
+import com.example.vegan.global.config.security.AuthenticationFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,17 +19,19 @@ public class LikeService {
     private final LikeRepository likeRepository;
     private final PostRepository postRepository;
     private final UserRepository userRepository;
+    private final AuthenticationFacade authenticationFacade;
 
-    private boolean isNotAlreadyLike(Post post){
-        return likeRepository.findByPost(post).isEmpty();
+    private boolean isNotAlreadyLike(Post post, User user){
+        return likeRepository.findByPostAndUser(post, user).isEmpty();
     }
 
     @Transactional
     public void insertLike(Long postId) {
+        User user = userRepository.findById((long) authenticationFacade.getUserId()).orElseThrow(() -> new IllegalArgumentException("not found"));
         Post post = postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("not found"));
-        Like like = new Like(post);
+        Like like = new Like(post, user);
 
-        if(isNotAlreadyLike(post)){
+        if(isNotAlreadyLike(post, user)){
             likeRepository.save(like);
         }
         else{
@@ -37,8 +41,9 @@ public class LikeService {
 
     @Transactional
     public void deleteLike(Long postId) {
+        User user = userRepository.findById((long) authenticationFacade.getUserId()).orElseThrow(() -> new IllegalArgumentException("not found"));
         Post post = postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("not found"));
-        Like like = likeRepository.findByPost(post).orElseThrow(() -> new IllegalArgumentException("not found"));
+        Like like = likeRepository.findByPostAndUser(post, user).orElseThrow(() -> new IllegalArgumentException("not found"));
         likeRepository.delete(like);
     }
 }
