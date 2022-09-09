3413ecfecaa88523c98b02fdc79f850b0fa4f2f7
wheeeung <whee050916@gmail.com>
2022-09-09 23:59:30 +0900

    📑 :: admin & report service development

--- src/main/java/com/example/vegan/domain/admin/controller/AdminController.java
+++ src/main/java/com/example/vegan/domain/admin/controller/AdminController.java
@@ -1,9 +1,21 @@
 package com.example.vegan.domain.admin.controller;
 
+import com.example.vegan.domain.admin.service.AdminService;
+import com.example.vegan.domain.report.entity.PostReport;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class AdminController {
+
+    private final AdminService adminService;
+
+    @GetMapping("/admin/report")
+    public List<PostReport> reportList(){
+        return adminService.reportList();
+    }
 }
--- src/main/java/com/example/vegan/domain/report/service/ReportService.java
+++ src/main/java/com/example/vegan/domain/report/service/ReportService.java
@@ -1,9 +1,25 @@
 package com.example.vegan.domain.report.service;
 
+import com.example.vegan.domain.post.entity.Post;
+import com.example.vegan.domain.post.repository.PostRepository;
+import com.example.vegan.domain.report.entity.PostReport;
+import com.example.vegan.domain.report.repository.PostReportRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ReportService {
+
+    private final PostReportRepository postReportRepository;
+    private final PostRepository postRepository;
+
+    @Transactional
+    public void report(Long id){
+        Post post = postRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("not found"));
+        PostReport postReport = new PostReport(post);
+        postReportRepository.save(postReport);
+    }
 }
