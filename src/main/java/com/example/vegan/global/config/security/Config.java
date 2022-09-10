9cb548a819808b453e6a4a092456e5ddd8ee3898
wheeeung <whee050916@gmail.com>
2022-09-10 11:33:41 +0900

    📑 :: change file

--- src/main/java/com/example/vegan/global/config/security/Config.java
+++ src/main/java/com/example/vegan/global/config/security/Config.java
@@ -6,6 +6,7 @@ import com.example.vegan.global.config.oauth.CustomOAuth2UserService;
 import com.example.vegan.global.config.oauth.OAuth2SuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,18 +21,29 @@ public class Config extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().disable()
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        http.csrf().disable().headers().frameOptions().disable()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/token/**").permitAll()
+                .antMatchers("/login**").permitAll()
+                .antMatchers("/admin/**").hasAnyRole("ADMIN")
+                .antMatchers("/board/**", "/user/**").hasAnyRole("USER")
                 .anyRequest().authenticated()
                 .and()
-                .oauth2Login().loginPage("/token/expired")
+                .logout().logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
                 .successHandler(successHandler)
                 .userInfoEndpoint().userService(oAuth2UserService);
-
-        http.addFilterBefore(new JwtAuthFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
+        http
+                .headers()
+                .frameOptions().sameOrigin()
+                .and()
+                .cors()
+                .and()
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(new JwtAuthFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
     }
 }
